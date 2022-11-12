56365b047e627aa2f053514106e12636493a3bf6
Lorince TAWAMBA <lorince.tawamba@gmail.com>
2022-11-12 08:48:09 +0100

    Lecture d'une propriété

--- src/tech/satic/demo/appli/Application.java
+++ src/tech/satic/demo/appli/Application.java
@@ -5,5 +5,7 @@ import java.util.Arrays;
 public class Application {
     public static void main(String[] args) {
         System.out.println("====== DEBUT DU PROGRAMME =======");
+        String name = System.getProperty("user.home");
+        System.out.println(name);
     }
 }
