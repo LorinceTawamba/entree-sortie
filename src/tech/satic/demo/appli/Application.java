48e8957b1f4260778c2799603f3c4d94e5bb2547
Lorince TAWAMBA <lorince.tawamba@gmail.com>
2022-11-12 08:40:49 +0100

    Initial commit

---
+++ src/tech/satic/demo/appli/Application.java
@@ -0,0 +1,9 @@
+package tech.satic.demo.appli;
+
+import java.util.Arrays;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("====== DEBUT DU PROGRAMME =======");
+    }
+}
